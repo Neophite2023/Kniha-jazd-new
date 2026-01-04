83b7c5b880fd649f8ae544d88a59a426181cdedb
Neophite2023 <132128221+Neophite2023@users.noreply.github.com>
2026-01-04 11:35:20 +0100

    Auto-deploy: Added GitHub Actions workflow and updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Dôležité pre GitHub Pages
+  base: '/Kniha-jazd-new/', // Názov repozitára na GitHub
   build: {
     outDir: 'dist',
   }
